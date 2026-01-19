d04e21704cf7966563c9ed37bc97f5d769a4bd12
Matthew Stengel <matthewstengel69@gmail.com>
2026-01-19 04:50:47 -0600

    up

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.1a2b04be8fe34bb1abd659ca66ba99f4',
+  appId: 'com.winterwatchapp',
   appName: 'WinterWatch-Pro',
-  webDir: 'dist',
-  server: {
-    url: 'https://1a2b04be-8fe3-4bb1-abd6-59ca66ba99f4.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
