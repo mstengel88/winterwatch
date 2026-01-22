9f944552de4db975fc824412cabe793347e1fa2b
mstengel88 <86675739+mstengel88@users.noreply.github.com>
2026-01-22 17:16:53 -0600

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,14 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      flowType: "pkce",
-      detectSessionInUrl: false,
-      persistSession: true,
-    },
+export { supabase } from "@/integrations/supabase/client";
   }
 );
 
